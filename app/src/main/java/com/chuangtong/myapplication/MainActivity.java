d8347802022c4ed7d5d9034643960838ef104f5c
zhangjihua396 <zhangjihua396@163.com>
2017-08-02 15:51:35 +0800

    1.首次提交

--- app/src/main/java/com/chuangtong/myapplication/MainActivity.java
+++ app/src/main/java/com/chuangtong/myapplication/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+        System.out.print("你好");
     }
 
     /**
